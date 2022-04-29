2b4b08d0b3d907f4583bd0f5b798b121cb6a9f6a
Valiantsin Kanapatski <Konopasies@yandex.by>
2022-04-29 09:48:00 +0300

    select is ready

--- src/components/UI/Select/Seclect.jsx
+++ src/components/UI/Select/Seclect.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component, createRef } from 'react';
 import cl from './Select.module.css'
 
 class Seclect extends Component {
@@ -6,36 +6,41 @@ class Seclect extends Component {
         super(props)
         this.state = { isSelectOpen: false}
     }
+    
     toggleSelect = (isSelectOpen) => {
         this.setState({isSelectOpen: isSelectOpen})
     }
-
-    componentDidMount() {
-        document.addEventListener('mousedown', this.closeSelect);
-        console.log('did');
-      }
-    
-    componentWillUnmount() {
-        document.removeEventListener('mousedown', this.closeSelect);
-        console.log('will');
-    }
-
 
     handleClick = (symbol) => {
         this.props.getCurrency(symbol)
         this.toggleSelect(false)
     }
 
-    
+    selectRef = createRef()
 
-    // closeSelect = () => {
-    //     this.setState({isSelectOpen: true})
-    // }
+    componentDidMount() {
+        document.addEventListener('mousedown', this.closeSelect);
+      }
+    
+    componentWillUnmount() {
+        document.removeEventListener('mousedown', this.closeSelect);
+    }
+
+    closeSelect = (event) => {
+        if (
+            this.selectRef.current &&
+            !this.selectRef.current.contains(event.target)
+          ) {
+            this.setState({isSelectOpen: false})
+          }
+        
+        
+    }
 
     render() {
         return (
-            <div className={cl.select}>
-                <div className={cl.header} onClick={() => {this.toggleSelect(this.state.isSelectOpen? false: true)}}>
+            <div className={cl.select} ref={this.selectRef}>
+                <div className={cl.header} onClick={() => {this.toggleSelect(this.state.isSelectOpen? false : true)}}>
                     <span className={cl.current}>{this.props.symbol}</span>
                     {
                         this.state.isSelectOpen
@@ -55,15 +60,6 @@ class Seclect extends Component {
                     </div>
                     : ''
                 }
-                
-                    
-                    {/* <div className={cl.body}>
-                    {this.props.currencies.map(cur => 
-                        <div className={cl.item} key={cur.symbol} onClick={(e) => this.handleClick(cur.symbol, e)}><span>{cur.symbol}</span><span>{cur.label}</span></div>
-                    )}
-                    </div> */}
-                    
-                
                 
             </div>
         );
